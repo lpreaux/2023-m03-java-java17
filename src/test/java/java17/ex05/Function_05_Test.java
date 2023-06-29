52b65671b6f86379fe1a997c70b985c75bfc2330
lucas PREAUX <lucas.preau@gmail.com>
2023-06-29 15:46:02 +0200

    feat(tp03-ex05): Functions - Interface Consumer

--- src/test/java/java17/ex05/Function_05_Test.java
+++ src/test/java/java17/ex05/Function_05_Test.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java17.data.Data;
 import java17.data.Person;
@@ -16,15 +17,15 @@ public class Function_05_Test {
     //tag::functions[]
     // TODO compléter la fonction
     // TODO modifier le mot de passe en "secret"
-    Consumer<Person> changePasswordToSecret = null;
+    Consumer<Person> changePasswordToSecret = person -> person.setPassword("secret");
 
     // TODO compléter la fonction
     // TODO vérifier que l'age > 4 avec une assertion JUnit
-    Consumer<Person> verifyAge = null;
+    Consumer<Person> verifyAge = person -> assertTrue(person.getAge() > 4);
 
     // TODO compléter la fonction
     // TODO vérifier que le mot de passe est "secret" avec une assertion JUnit
-    Consumer<Person> verifyPassword = null;
+    Consumer<Person> verifyPassword = person -> assertEquals("secret", person.getPassword());
     //end::functions[]
 
 
@@ -34,13 +35,11 @@ public class Function_05_Test {
 
         // TODO invoquer la méthode personList.forEach pour modifier les mots de passe en "secret"
         // personList.forEach...
+        personList.forEach(changePasswordToSecret);
 
         // TODO remplacer la boucle for par l'invocation de la méthode forEach
         // TODO Utiliser la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
         // personList.forEach...
-        for(Person p : personList) {
-            verifyAge.accept(p);
-            verifyPassword.accept(p);
-        }
+        personList.forEach(verifyAge.andThen(verifyPassword));
     }
 }
