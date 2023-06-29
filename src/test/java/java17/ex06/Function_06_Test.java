7b9b0b1bf500bb978285cdf54d91db94245530a7
lucas PREAUX <lucas.preau@gmail.com>
2023-06-29 15:49:12 +0200

    feat(tp03-ex06): Functions - Interface Supplier

--- src/test/java/java17/ex06/Function_06_Test.java
+++ src/test/java/java17/ex06/Function_06_Test.java
@@ -18,7 +18,7 @@ public class Function_06_Test {
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
         // TODO
-        return null;
+        return "[age=%s]".formatted(supplier.get().getAge());
     }
     // end::formatAge[]
 
@@ -26,7 +26,7 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+        String result = formatAge(() -> new Person("first", "last", 35, "pass"));
 
         assert result.equals("[age=35]");
     }
